d055639cb3791e99a5ef69f9e3813f00686d10c0
EaudeJavel <p.plamy6@gmail.com>
2022-06-07 23:20:36 +0200

    new: Array.diff

--- main.js
+++ main.js
@@ -140,3 +140,27 @@ const moveZeros = arr => [
   ...arr.filter(n => n !== 0),
   ...arr.filter(n => n === 0)
 ];
+
+
+// Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+function arrayDiff(a, b) {
+  const c = a.values()
+  const e = []
+
+  for(const i of c) {
+    if (!b.includes(i)) {
+      e.push(i)
+    }
+  }
+  return e
+}
+
+//other
+function array_diff(a, b) {
+  return a.filter(e => !b.includes(e));
+}
